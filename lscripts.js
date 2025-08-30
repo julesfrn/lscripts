eb106bcdd431fbdc8e2427840c3dc86a1ef18c92
Jules Fournier <44777840+julesfrn@users.noreply.github.com>
2025-08-30 12:27:37 +0200

    feat: add copy flag, version flag and help flag (#6)

--- lscripts.js
+++ lscripts.js
@@ -3,19 +3,71 @@
 import inquirer from 'inquirer'
 import inquirerPrompt from 'inquirer-autocomplete-prompt'
 import fs from 'fs'
-import { exit } from 'process'
+import { exit, argv } from 'process'
 import { spawn } from 'child_process'
+import clipboard from 'clipboardy'
+import lscriptsPackageJson from './package.json' with { type: "json" }
 
 const rootDir = '/'
 
+let isCopyModeEnabled = false
+const useCopyMode = () => {
+  isCopyModeEnabled = true
+}
+
+const displayHelpMessage = () => {
+  console.log(`Usage: lscripts [option]
+Simply list and run a script form the package.json of your current node project.
+
+ option      alias   description
+----------- ------- -----------------------------------------------------------------
+ --copy      -c      Enables copy mode: when you pick your script, it will be copied
+                     to your clipboard instead of being ran immediately
+ --version   -v      Displays your installed version of lscripts
+ --help      -h      Displays this help message
+
+You can only pass one argument at a time to lscripts command.`)
+  exit(0)
+}
+
+const displayVersion = () => {
+  console.log(`You are currently using lscripts version ${lscriptsPackageJson.version}`)
+  exit(0)
+}
+
+const possibleFlags = [
+  { flags: ['--copy', '-c'], callback: useCopyMode },
+  { flags: ['--help', '-h'], callback: displayHelpMessage },
+  { flags: ['--version', '-v'], callback: displayVersion }
+]
+
+const parseArguments = () => {
+  if (argv.length < 3) return
+  if (argv.length > 3) {
+    console.log('You can only pass one argument at a time to lscripts.')
+    console.log(`Run 'lscripts --help' for more information.`)
+    return exit(1)
+  }
+
+  const arg = argv[2]
+  const flag = possibleFlags.find(({ flags }) => flags.includes(arg))
+
+  if (!flag) {
+    console.log(`Unkown argument "${arg}".`)
+    console.log(`Run 'lscripts --help' for more information.`)
+    return exit(1)
+  }
+
+  flag.callback()
+}
+
 const findPackageJsonInWorkingDirOrParent = (currentDir = './') => {
-  if (fs.existsSync(`${currentDir}package.json`)) return fs.readFileSync(`${currentDir}package.json`)
+  if (fs.existsSync(`${currentDir}package.json`)) return JSON.parse(fs.readFileSync(`${currentDir}package.json`))
   if (fs.realpathSync(currentDir) === rootDir) return null
   return findPackageJsonInWorkingDirOrParent(`${currentDir}../`)
 }
 
-const getScriptsFromPackageJson = (packageJson) =>
-  Object.entries(JSON.parse(packageJson).scripts)
+const getScriptsFromPackageJson = (packageJson) => Object.entries(packageJson.scripts)
 
 const getPackageManagerEngine = (currentDir = './') => {
   if (fs.existsSync(`${currentDir}package-lock.json`)) return 'npm'
@@ -24,6 +76,8 @@ const getPackageManagerEngine = (currentDir = './') => {
   if (fs.realpathSync(currentDir) === rootDir) return null
   return getPackageManagerEngine((`${currentDir}../`))
 }
+
+parseArguments()
 
 const packageJson = findPackageJsonInWorkingDirOrParent()
 
@@ -58,18 +112,24 @@ inquirer.registerPrompt('autocomplete', inquirerPrompt)
 inquirer.prompt({
   type: 'autocomplete',
   name: 'script',
-  message: 'What script do you want to copy to your clipboard ?',
+  message: isCopyModeEnabled
+    ? 'What script do you want to copy to your clipboard ?'
+    : 'What script do you want to run ?',
   emptyText: `Sorry, I couldn't find the script you are looking for...`,
   searchText: 'Looking for the script...',
   pageSize: scripts.length,
   source
 }).then(({ script }) => {
-  const child = spawn(runner, ['run', script.match(regex)[1]], {
-    stdio: 'inherit',
-    shell: true
-  })
+  const command = `🚀 ${runner} run ${script.match(regex)[1]}`
 
-  child.on('exit', (code) => {
-    process.exit(code);
-  })
+  if (isCopyModeEnabled) {
+    clipboard.writeSync(command)
+    console.log('✅ Command copied to your clipboard!')
+    console.log('You can paste it and run it!')
+    return
+  }
+
+  console.log(command)
+  spawn(command, { stdio: 'inherit', shell: true })
+    .on('exit', process.exit)
 })
